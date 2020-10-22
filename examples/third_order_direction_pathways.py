da2dcb54b21ab791e98a5f6a3d1ca1372142ad7a
Grzegorz Kowzan <grzegorz@kowzan.eu>
2020-10-22 10:58:17 +0200

    add comparison of signals for different pathways

--- examples/third_order_direction_pathways.py
+++ examples/third_order_direction_pathways.py
@@ -1,35 +1,36 @@
+"""Compare rotationally-resolved signals for different pathways and directions."""
 # * Imports and functions
-import numpy as np
-import scipy.constants as C
+import time
+from pathlib import Path
+
 import matplotlib.pyplot as plt
-import spectroscopy.foreign.hapi3 as h3
+import numpy as np
 import pyfftw.interfaces.scipy_fftpack as fftp
-from spectroscopy import happier
+import rotsim2d.pathways as pw
+import rotsim2d.propagate as prop
+import rotsim2d.visual as vis
 import shed.units as u
 from shed.experiment import find_index
-import rotsim2d.propagate as prop
-import rotsim2d.pathways as pw
-import rotsim2d.visual as vis
+from spectroscopy import happier
 plt.ion()
+OUTPUT = Path('/mnt/d/DFCS/Stony Brook/rotsim2d_results/carbon_monoxide')
 
 # * System properties
-pressure, Tgas, length = 5.0, 296.0, 3.0
+pressure, Tgas, length = 20.0, 296.0, 3.0
 conc = 1e-7*happier.volumeConcentration(pressure, Tgas)*1e6
 co_levels = happier.energy_levels([0,1,2], 5, 1)
 co_pops = happier.equilibrium_pops(co_levels, Tgas, 5, 1)
-co_params = happier.line_params([(0, 1), (1, 2), (2, 3)], 5, 1)
+co_params = happier.line_params([(0, 1), (1, 2), (2, 3), (0, 2)], 5, 1)
+co_params_suppl = happier.generate_line_params(
+    [(0, 0, 0), (1, 1, 0), (2, 2, 0), (0, 0, 2), (1, 1, 2), (0, 2, 2), (0, 2, 0)], co_levels, co_params)
+co_params.update(co_params_suppl)
 co_env = {'T': Tgas, 'p': pressure}
-co_params = happier.apply_env(co_params, co_env)
-co_params = {k: {'mu': v['mu'], 'gam': u.wn2nu(v['gam']), 'nu': u.wn2nu(v['nu'])}
-             for k, v in co_params.items()}
+co_params = happier.apply_env(co_params, co_env, to_nu=True)
 
 # * Response function parameters
 dt = 1.0/(2.0*u.wn2nu(343.0))/2.0
-T = 1.0/co_params[((0,0), (1,1))]['gam']*3.0/5.0         # relaxation time constant
-# dt = 1e-15
-# T = 1e-12
+T = 1.0/co_params[((0,0), (1,1))]['gam']*4.0/5.0         # relaxation time constant
 N = np.round(T/dt).astype(np.int)
-#t1 = 1/co_params[((0,0), (1,1))]['gam']/20.0
 t2 = 0.0
 ts = np.arange(N)*dt
 
@@ -39,12 +40,9 @@ root_prop = prop.Propagator({'elevels': co_levels, 'populations': co_pops,
                             # filter=lambda kb: kb.parent.parent==pw.KetBra(0, 0, 1, 1)\
                             # or kb.parent.parent==pw.KetBra(1, 1, 0, 0),
                             filter=lambda kb: True,
-                            freq_shift=u.wn2nu(1800.0))
+                            freq_shift=[u.wn2nu(1800.0)])
 resp_xs_freqs = root_prop.cross_section_freqs([ts])
 resp_xs_freqs_wn = u.nu2wn(resp_xs_freqs)
-wnmin, wnmax = 240, 430
-imin, imax = find_index(resp_xs_freqs_wn, wnmin), find_index(resp_xs_freqs_wn, wnmax)
-Nsmall = imax-imin
 
 # ** First order
 resp_xs_spec_o1 = np.zeros(ts.size, dtype=np.complex)
@@ -53,146 +51,131 @@ for j in range(5, 6):
     root = pw.KetBra(0, j, 0, j)
     root = pw.excite(root, 'omg1', 'both')
     root = pw.readout(root)
-    pop_factor = co_pops[(0, j)]/(2*j+1)
+    pop_factor = co_pops[(0, j)]
     resp_xs_spec_o1 += pop_factor*root_prop.cross_section_spectrum(root, [ts])
 
 # ** Third order
-import time
+root_prop = prop.Propagator({'elevels': co_levels, 'populations': co_pops,
+                             'line_params': co_params},
+                            # filter=lambda kb: kb.parent.parent==pw.KetBra(0, 0, 1, 1)\
+                            # or kb.parent.parent==pw.KetBra(1, 1, 0, 0),
+                            filter=lambda kb: True,
+                            freq_shift=[u.wn2nu(1800.0), 0.0, u.wn2nu(1800.0)])
+
 resp_xs = np.zeros((ts.size, ts.size), dtype=np.complex)
-for j in range(5, 6):
+for j in range(0, 16):
     # third order
     print(j, time.asctime())
     root = pw.KetBra(0, j, 0, j)
     root = pw.multi_excite(root, ['omg1', 'omg2', 'omg3'],
-                           parts=['both', 'both', 'both'])
+                           parts=['ket', 'both', 'both'])
     # root = pw.excite(root, 'omg1', 'both')
-    root = pw.remove_nonrephasing(root)
+    # root = pw.remove_interstate(root)
     root = pw.readout(root)
-    pop_factor = co_pops[(0, j)]/(2*j+1)
+    pop_factor = co_pops[(0, j)]
     for i, t1 in enumerate(ts):
         resp_xs[i, :] += pop_factor*root_prop.response(root, [t1, t2, ts])
 
 # *** Fourier transform in pump axis
-resp_xs_spec_fft = fftp.fft2(resp_xs, planner_effort='FFTW_ESTIMATE')
-# resp_xs_spec_fft = np.zeros((ts.size, ts.size), dtype=np.complex)
-# for i in range(ts.size):
-#     resp_xs_spec_fft[:, i] = fftp.fft2(resp_spec[:, i],
-#                                        planner_effort='FFTW_ESTIMATE')
+resp_xs_freqs_wn = fftp.fftshift(u.nu2wn(root_prop.cross_section_freqs([ts])))
+resp_xs_spec_fft = fftp.fftshift(fftp.fft2(resp_xs, planner_effort='FFTW_ESTIMATE'))
+if resp_xs_freqs_wn.size % 2 == 0:
+    # Make positive and negative frequencies symmetric around DC.
+    resp_xs_freqs_wn = resp_xs_freqs_wn[1:]
+    resp_xs_spec_fft = resp_xs_spec_fft[1:, 1:]
+# Invert the order elements to agree with Mukamel's FT convention.
+resp_xs_spec_fft = resp_xs_spec_fft[::-1, ::-1]
 
 # *** Plot
-fig_dict = vis.plot2d_im(freqs=(resp_xs_freqs_wn[imin:imax],
-                                resp_xs_freqs_wn[imin:imax]),
-                         spec2d=np.real(resp_xs_spec_fft[1:,1:]),
-                         spec_linear=(resp_xs_freqs_wn[imin:imax],
-                                      np.imag(resp_xs_spec_o1[imin:imax])))
+fig_dict = vis.plot2d_im(freqs=(resp_xs_freqs_wn+1800.0, resp_xs_freqs_wn+1800.0),
+                         spec2d=prop.absorptive(resp_xs_spec_fft),
+                         scale='linear')
+fig_dict['fig'].canvas.set_window_title('Time domain -- absorptive -- all pathways')
+fig_dict['ax2d'].set(xlim=(2000, 2250), ylim=(2000, 2250))
+fig_dict['fig'].savefig(str(OUTPUT / 'mandal_full_linear.png'))
 
-fig_dict = vis.plot2d_im(freqs=(resp_xs_freqs_wn[imin:imax],
-                                resp_xs_freqs_wn[imin:imax]),
-                         spec2d=np.real(resp_xs_spec_fft + resp_xs_spec_fft[::-1]),
-                         spec_linear=(resp_xs_freqs_wn[imin:imax],
-                                      np.imag(resp_xs_spec_o1[imin:imax])))
+# ** Third order -- nointerstates
+root_prop = prop.Propagator({'elevels': co_levels, 'populations': co_pops,
+                             'line_params': co_params},
+                            # filter=lambda kb: kb.parent.parent==pw.KetBra(0, 0, 1, 1)\
+                            # or kb.parent.parent==pw.KetBra(1, 1, 0, 0),
+                            filter=lambda kb: True,
+                            freq_shift=[u.wn2nu(1800.0), 0.0, u.wn2nu(1800.0)])
 
-# ** Third order -- no ESA
-import time
-resp_xs_spec_no_esa = np.zeros((ts.size, Nsmall), dtype=np.complex)
-for j in range(5, 6):
+resp_xs = np.zeros((ts.size, ts.size), dtype=np.complex)
+for j in range(0, 16):
     # third order
     print(j, time.asctime())
     root = pw.KetBra(0, j, 0, j)
     root = pw.multi_excite(root, ['omg1', 'omg2', 'omg3'],
-                           parts=['both', 'both', 'both'])
+                           parts=['ket', 'both', 'both'])
     # root = pw.excite(root, 'omg1', 'both')
-    root = pw.remove_esa(root)
+    root = pw.remove_interstates(root)
     root = pw.readout(root)
-    pop_factor = co_pops[(0, j)]/(2*j+1)
+    pop_factor = co_pops[(0, j)]
     for i, t1 in enumerate(ts):
-        resp_xs_spec_no_esa[i, :] += pop_factor*root_prop.cross_section_spectrum(root, [t1, t2, ts])[imin:imax]
+        resp_xs[i, :] += pop_factor*root_prop.response(root, [t1, t2, ts])
 
 # *** Fourier transform in pump axis
-resp_xs_spec_no_esa_fft = np.zeros((ts.size, Nsmall), dtype=np.complex)
-for i in range(Nsmall):
-    resp_xs_spec_no_esa_fft[:, i] = fftp.fft(resp_xs_spec_no_esa[:, i],
-                                             planner_effort='FFTW_ESTIMATE')
+resp_xs_freqs_wn = fftp.fftshift(u.nu2wn(root_prop.cross_section_freqs([ts])))
+resp_xs_spec_fft_noint = fftp.fftshift(fftp.fft2(resp_xs, planner_effort='FFTW_ESTIMATE'))
+if resp_xs_freqs_wn.size % 2 == 0:
+    # Make positive and negative frequencies symmetric around DC.
+    resp_xs_freqs_wn = resp_xs_freqs_wn[1:]
+    resp_xs_spec_fft_noint = resp_xs_spec_fft_noint[1:, 1:]
+# Invert the order elements to agree with Mukamel's FT convention.
+resp_xs_spec_fft_noint = resp_xs_spec_fft_noint[::-1, ::-1]
 
 # *** Plot
-fig_dict = vis.plot2d_im(freqs=(resp_xs_freqs_wn[imin:imax],
-                                resp_xs_freqs_wn[imin:imax]),
-                         spec2d=np.imag(resp_xs_spec_no_esa_fft[imin:imax, :][::-1]),
-                         spec_linear=(resp_xs_freqs_wn[imin:imax],
-                                      np.imag(resp_xs_spec_o1[imin:imax])))
+fig_dict = vis.plot2d_im(freqs=(resp_xs_freqs_wn+1800.0, resp_xs_freqs_wn+1800.0),
+                         spec2d=prop.absorptive(resp_xs_spec_fft_noint),
+                         scale='linear')
+fig_dict['fig'].canvas.set_window_title('Time domain -- absorptive -- no interstates')
+fig_dict['ax2d'].set(xlim=(2000, 2250), ylim=(2000, 2250))
+fig_dict['fig'].savefig(str(OUTPUT / 'mandal_nointerstates_linear.png'))
 
+# ** Third order -- no overtones
+root_prop = prop.Propagator({'elevels': co_levels, 'populations': co_pops,
+                             'line_params': co_params},
+                            # filter=lambda kb: kb.parent.parent==pw.KetBra(0, 0, 1, 1)\
+                            # or kb.parent.parent==pw.KetBra(1, 1, 0, 0),
+                            filter=lambda kb: True,
+                            freq_shift=[u.wn2nu(1800.0), 0.0, u.wn2nu(1800.0)])
 
-# ** Third order -- nonrephasing
-import time
-resp_xs_spec_nonreph = np.zeros((ts.size, Nsmall), dtype=np.complex)
-for j in range(5, 6):
+resp_xs = np.zeros((ts.size, ts.size), dtype=np.complex)
+for j in range(0, 16):
     # third order
     print(j, time.asctime())
     root = pw.KetBra(0, j, 0, j)
     root = pw.multi_excite(root, ['omg1', 'omg2', 'omg3'],
-                           parts=['both', 'both', 'both'])
-    root = pw.remove_rephasing(root)
+                           parts=['ket', 'both', 'both'])
+    # root = pw.excite(root, 'omg1', 'both')
+    root = pw.remove_overtones(root)
     root = pw.readout(root)
-    pop_factor = co_pops[(0, j)]/(2*j+1)
+    pop_factor = co_pops[(0, j)]
     for i, t1 in enumerate(ts):
-        resp_xs_spec_nonreph[i, :] += pop_factor*root_prop.cross_section_spectrum(root, [t1, t2, ts])[imin:imax]
+        resp_xs[i, :] += pop_factor*root_prop.response(root, [t1, t2, ts])
 
 # *** Fourier transform in pump axis
-resp_xs_spec_nonreph_fft = np.zeros((ts.size, Nsmall), dtype=np.complex)
-for i in range(Nsmall):
-    resp_xs_spec_nonreph_fft[:, i] = fftp.fft(resp_xs_spec_nonreph[:, i],
-                                              planner_effort='FFTW_ESTIMATE')
+resp_xs_freqs_wn = fftp.fftshift(u.nu2wn(root_prop.cross_section_freqs([ts])))
+resp_xs_spec_fft_noover = fftp.fftshift(fftp.fft2(resp_xs, planner_effort='FFTW_ESTIMATE'))
+if resp_xs_freqs_wn.size % 2 == 0:
+    # Make positive and negative frequencies symmetric around DC.
+    resp_xs_freqs_wn = resp_xs_freqs_wn[1:]
+    resp_xs_spec_fft_noover = resp_xs_spec_fft_noover[1:, 1:]
+# Invert the order elements to agree with Mukamel's FT convention.
+resp_xs_spec_fft_noover = resp_xs_spec_fft_noover[::-1, ::-1]
 
 # *** Plot
-fig_dict = vis.plot2d_im(freqs=(resp_xs_freqs_wn[imin:imax],
-                                resp_xs_freqs_wn[imin:imax]),
-                         spec2d=np.real(resp_xs_spec_nonreph_fft[::-1]),
-                         spec_linear=(resp_xs_freqs_wn[imin:imax],
-                                      np.imag(resp_xs_spec_o1[imin:imax])))
+fig_dict = vis.plot2d_im(freqs=(resp_xs_freqs_wn+1800.0, resp_xs_freqs_wn+1800.0),
+                         spec2d=prop.absorptive(resp_xs_spec_fft_noover),
+                         scale='linear')
+fig_dict['fig'].canvas.set_window_title('Time domain -- absorptive -- no overtones')
+fig_dict['ax2d'].set(xlim=(2000, 2250), ylim=(2000, 2250))
+fig_dict['fig'].savefig(str(OUTPUT / 'mandal_noovertones_linear.png'))
 
-# ** Third order -- rephasing
-import time
-resp_xs_spec_reph = np.zeros((ts.size, Nsmall), dtype=np.complex)
-for j in range(5, 6):
-    # third order
-    print(j, time.asctime())
-    root = pw.KetBra(0, j, 0, j)
-    root = pw.multi_excite(root, ['omg1', 'omg2', 'omg3'],
-                           parts=['both', 'both', 'both'])
-    root = pw.remove_nonrephasing(root)
-    root = pw.readout(root)
-    pop_factor = co_pops[(0, j)]/(2*j+1)
-    for i, t1 in enumerate(ts):
-        resp_xs_spec_reph[i, :] += pop_factor*root_prop.cross_section_spectrum(root, [t1, t2, ts])[imin:imax]
-
-# *** Fourier transform in pump axis
-resp_xs_spec_reph_fft = np.zeros((ts.size, Nsmall), dtype=np.complex)
-for i in range(Nsmall):
-    resp_xs_spec_reph_fft[:, i] = fftp.fft(resp_xs_spec_reph[:, i],
-                                           planner_effort='FFTW_ESTIMATE')
-
-# *** Plot
-fig_dict = vis.plot2d_im(freqs=(resp_xs_freqs_wn[imin:imax],
-                                resp_xs_freqs_wn[imin:imax]),
-                         spec2d=np.imag(resp_xs_spec_reph_fft[::-1]),
-                         spec_linear=(resp_xs_freqs_wn[imin:imax],
-                                      np.imag(resp_xs_spec_o1[imin:imax])))
-
-# ** Third order -- rephasing-nonrephasing
-# *** Plot
-fig_dict = vis.plot2d_im(freqs=(resp_xs_freqs_wn[imin:imax],
-                                resp_xs_freqs_wn[imin:imax]),
-                         spec2d=np.imag(resp_xs_spec_reph_fft[::-1]+
-                                        resp_xs_spec_nonreph_fft),
-                         spec_linear=(resp_xs_freqs_wn[imin:imax],
-                                      np.imag(resp_xs_spec_o1[imin:imax])))
-
-# * Graph
-root = pw.KetBra(0, 5, 0, 5)
-root = pw.multi_excite(root, ['omg1', 'omg2', 'omg3'])
-root = pw.readout(root)
-root = pw.remove_nondiagonal(root)
-root.savepng('/mnt/c/Users/heroy/Desktop/third_order_readout.png')
-
-root = pw.remove_rephasing(root)
-root.savepng('/mnt/c/Users/heroy/Desktop/third_order_no_esa.png')
+# ** Compare w/ and wo/ overtones
+fig_dict = vis.plot2d_im(freqs=(resp_xs_freqs_wn+1800.0, resp_xs_freqs_wn+1800.0),
+                         spec2d=np.imag(resp_xs_spec_fft-resp_xs_spec_fft_noover))
+fig_dict['fig'].suptitle('purely absorptive')
+fig_dict['fig'].canvas.set_window_title('Time domain -- absorptive')
