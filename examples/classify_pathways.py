06a452c0149043f7c00ee3d00eeb6e6408b480fb
Grzegorz Kowzan <grzegorz@kowzan.eu>
2021-04-27 16:40:24 -0400

    move classify_pathways example

--- examples/classify_pathways.py
+++ examples/classify_pathways.py
@@ -1,15 +1,10 @@
 # * Imports
 import matplotlib.pyplot as plt
-from molspecutils.molecule import CH3ClAlchemyMode
 import rotsim2d.pathways as pw
 import rotsim2d.dressedleaf as dl
-import rotsim2d.angular.symbolic as sym
-import rotsim2d.angular.symbolic_results as symr
+import rotsim2d.symbolic.functions as sym
+import rotsim2d.symbolic.results as symr
 plt.ion()
-
-# * Vibrational mode
-ch3cl_mode = CH3ClAlchemyMode()
-T = 296.0
 
 # * Classify pathways
 # - add/remove pw.only_twocolor to switch between two-color and three-color
@@ -24,7 +19,9 @@ classified = sym.classify_dls(pws, rfactors)
 classified_states = {k: [pw.leaf.to_statelist(diatom=True, normalize=True) for pw in v]
                      for k, v in classified.items()}
 
+print("Polarization response expressions")
 dl.print_dl_dict(classified, fields=['peak', 'angles', 'geo_label', 'trans_label', 'tw_coherence'])
+print()
 
 # * Find zeroing angles
 zeroing_angles_gk = sym.suppression_angles(classified.keys(), [0, sym.pi/4, sym.pi/2])
@@ -37,4 +34,5 @@ angles_pws_vaccaro = sym.classify_suppression(classified, zeroing_angles_vaccaro
 
 # this will print out expressions for polarization dependence and pathways which
 # correspond to each expression
+print("Suppression angles:")
 dl.print_dl_dict(angles_pws_vaccaro, fields=['peak', 'angles', 'geo_label', 'trans_label', 'tw_coherence'])
