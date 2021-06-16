60522ca90f44951ff2a4001067cf24e5447fb468
Grzegorz Kowzan <grzegorz@kowzan.eu>
2021-06-16 11:27:12 -0400

    add conv argument to pws_autospan

--- rotsim2d/propagate.py
+++ rotsim2d/propagate.py
@@ -30,17 +30,22 @@ def aid(x):
     return x.__array_interface__['data'][0]
 
 
-def pws_autospan(pws, margin=5.0*30e9):
+def pws_autospan(pws: Sequence[dl.DressedPathway], margin: float=5.0*30e9,
+                 conv: float=1.0) -> Tuple[Tuple[float]]:
     """Return min/max pump/probe frequencies from `pws`."""
     pump_freqs, probe_freqs = [pw.nu(0) for pw in pws], [pw.nu(2) for pw in pws]
     pump_min, pump_max = min(pump_freqs), max(pump_freqs)
     probe_min, probe_max = min(probe_freqs), max(probe_freqs)
 
-    return ((pump_min-margin, pump_max+margin, 2*margin+pump_max-pump_min),
-            (probe_min-margin, probe_max+margin, 2*margin+probe_max-probe_min))
+    return ((pump_min*conv - margin, pump_max*conv + margin, 2*margin + conv*(pump_max-pump_min)),
+            (probe_min*conv - margin, probe_max*conv + margin, 2*margin + conv*(probe_max-probe_min)))
 
 
 def aligned_fs(fsmin: float, fsmax: float, df: float):
+    """Return `df`-spaced grid of values between `fsmin` and `fsmax`.
+
+    Align endpoints if they're not separated by a multiple of `df`.
+    """
     def align(f: float):
         return np.ceil(f/df).astype(np.int) if f < 0 else np.floor(f/df).astype(np.int)
     return np.arange(align(fsmin), align(fsmax)+1)*df
