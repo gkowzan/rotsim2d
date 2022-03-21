d27d233296171f55d67fd6889a2933be5974d2b8
Grzegorz Kowzan <grzegorz@kowzan.eu>
2022-03-21 13:32:45 -0400

    tests for cosmetic functions

--- tests/test_ketbras.py
+++ tests/test_ketbras.py
@@ -1,6 +1,7 @@
 """Some sanity checks for ketbras."""
 import pytest
-from anytree.iterators.preorderiter import PreOrderIter 
+from anytree.iterators.preorderiter import PreOrderIter
+from anytree.walker import WalkError, Walker
 import rotsim2d.pathways as pw
 import molspecutils.molecule as mol
 
@@ -15,9 +16,39 @@ def test_leafs_are_ketbras(kbs_tree):
         assert isinstance(l, pw.KetBra)
 
 
+def test_first_interaction_is_absorption(kbs_tree):
+    for li in kbs_tree.root.children:
+        assert li.is_absorption()
+
+
 def test_four_interactions(kbs_tree):
     for l in kbs_tree.leaves:
         assert len(l.interactions()) == 4
+
+
+def test_flip_readout(kbs_tree):
+    for l in kbs_tree.leaves:
+        side = l.parent.side
+        absorption = l.parent.is_absorption()
+        pw.flip_readout(l)
+        assert l.parent.side != side
+        assert l.parent.is_absorption() != absorption
+
+
+def test_conjugate_chain(kbs_tree):
+    conj = pw.conjugate_chain(kbs_tree.leaves[0])
+    orig_chain = [kbs_tree.leaves[0]] + list(kbs_tree.ancestors)
+    conj_chain = [conj] + list(conj.ancestors)
+    for o, c in zip(orig_chain, conj_chain):
+        assert o == c.conj()
+    with pytest.raises(WalkError):
+        walker = Walker()
+        walker.walk(conj, kbs_tree.leaves[0])
+
+
+def test_ketbra_evolve(kbs_tree):
+    for l in kbs_tree.leaves:
+        assert l.evolve() == l
 
 
 def test_five_ketbras(kbs_tree):
